3f7bd26784bcf98238677050fbcd34fe58e5fed7
Tian Chen <tian_chen@urmc.rochester.edu>
2011-12-03 16:04:51 -0500

    1. change package version 2. edited fastperm.c: index problem but not done yet, still not match the result of fastperm.R

--- src/fastperm.c
+++ src/fastperm.c
@@ -3,21 +3,26 @@
 /*#include <Rmath.h>*/
 #include <R.h>
 
-void fastperm3(double *X0,double *X1,double *X2,double *XtildePerm,int *B,int *N,double *max_array, double *min_array){
-  int i=0,m1=0,m2=0,BB=*B,NN=*N;
+void fastperm3(double *X0,double *X1,double *X2,double *XtildePerm,int *B,int *N,int *L,
+               double *maxvec, double *minvec){
+  int i=0,j=0,m1=0,m2=0,BB=*B,NN=*N,ind=0,LL=*L;
   double tmax=0,tmin=0;
   for (m1=0;m1<BB;m1++){
     for (i=0;i<NN;i++) {
-      XtildePerm[i]=X0[i]+X1[i+m1];
+      ind=(i+LL*m1) % NN;
+      ind=((ind == 0) ? NN:ind);
+      XtildePerm[i]=X0[i]+X1[ind];
     }
     for (m2=0;m2<BB;m2++){
-      for (i=0;i<NN;i++){
-        XtildePerm[i]+=X2[i+m2];
-        tmin=((XtildePerm[i] < tmin) ? XtildePerm[i]:tmin);
-        tmax=((XtildePerm[i] > tmax) ? XtildePerm[i]:tmax);
+      for (j=0;j<NN;j++){
+        ind=(j+LL*m2) % NN;
+        ind=((ind == 0) ? NN:ind);
+        XtildePerm[j]+=X2[ind];
+        tmin=((XtildePerm[j] < tmin) ? XtildePerm[j]:tmin);
+        tmax=((XtildePerm[j] > tmax) ? XtildePerm[j]:tmax);
       }
-      max_array[(m1-1)*BB+m2]=tmax;
-      min_array[(m1-1)*BB+m2]=tmin;
+      maxvec[m1*BB+m2]=tmax;
+      minvec[m1*BB+m2]=tmin;
     }
   }
 }
