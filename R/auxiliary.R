14910fac6729c03b3ec5d2ae31f693f4f2c9217e
Tian Chen <tian_chen@urmc.rochester.edu>
2011-12-04 02:23:03 -0500

    The fastperm3.C now can give exactly the same result as fastper3.R. Future work:(1) in fastper3.C, the initial value of tmin and tmax should be improved; (2)clean the input, some are not necessary, check fastperm3.R and fastperm3.C; (3)shift() and GaussFilterMat() wrapped in the function, trying to make example clear and simple

--- R/auxiliary.R
+++ R/auxiliary.R
@@ -1,12 +1,12 @@
 ## fastperm3.R version
-fastperm3.R <- function(X,t.min,t.max,L=3){
-  N<-length(X);B<-N/L;ind<-seq(0,N-1,L)
-  XX <- matrix(X,ncol=L,byrow=T)
-  Xtilde <- matrix(0,nrow=N,ncol=L)
-  for (l in 1:L){
+fastperm3.R <- function(X,t.min,t.max){
+  N<-length(X);B<-N/3;ind<-seq(0,N-1,3)
+  XX <- matrix(X,ncol=3,byrow=T)
+  Xtilde <- matrix(0,nrow=N,ncol=3)
+  for (l in 1:3){
     Xtilde[,l] <- as.vector(kermat[,ind1+l]%*%XX[,l])
   }
-  Xt0 <- Xtilde[,1];Xt1 <- Xtilde[,2]; Xt2 <- Xtilde[,3] # should change if L is not 3
+  Xt0 <- Xtilde[,1];Xt1 <- Xtilde[,2]; Xt2 <- Xtilde[,3]
   ## R version
   for (m1 in 1:B){
     Xt1 <- shift(Xt1,3)
@@ -24,8 +24,8 @@ fastperm3.R <- function(X,t.min,t.max,L=3){
 
 
 ## fastperm3.C version
-fastperm3.C <- function(X,t.min,t.max,L=3){
-  N<-length(X);B<-N/3;ind<-seq(0,N-1,3)
+fastperm3.C <- function(X,t.mim,t.max,L=3){
+  N<-length(X);B<-N/L;ind<-seq(0,N-1,3)
   XX <- matrix(X,ncol=3,byrow=T)
   Xtilde <- matrix(0,nrow=N,ncol=3)
   for (l in 1:3){
@@ -33,21 +33,12 @@ fastperm3.C <- function(X,t.min,t.max,L=3){
   }
   Xt0 <- Xtilde[,1];Xt1 <- Xtilde[,2]; Xt2 <- Xtilde[,3]
   ## implemented in C
-  XtildePerm <- rep(0,N)
-#  t.mins=.C("fastperm3",as.double(Xt0),as.double(Xt1),as.double(Xt2),
-#     as.double(XtildePerm), as.integer(B),as.integer(N),
-#     as.double(t.max),as.double(t.min),PACKAGE="flash")$t.min
-#  t.maxs=.C("fastperm3",as.double(Xt0),as.double(Xt1),as.double(Xt2),
-#     as.double(XtildePerm), as.integer(B),as.integer(N),
-#     as.double(t.max),as.double(t.min),PACKAGE="flash")$t.max
-   MaxVec <- .C("fastperm3",as.double(Xt0),as.double(Xt1),as.double(Xt2),
-     XtildePerm=as.double(XtildePerm), as.integer(B),as.integer(N),as.integer(L),
-     maxvec=as.double(t.max),minvec=as.double(t.min),PACKAGE="flash")$maxvec
-   Xtildeperm <- .C("fastperm3",as.double(Xt0),as.double(Xt1),as.double(Xt2),
-     XtildePerm=as.double(XtildePerm), as.integer(B),as.integer(N),as.integer(L),
-     maxvec=as.double(t.max),minvec=as.double(t.min),PACKAGE="flash")$XtildePerm
-   MinVec <- .C("fastperm3",as.double(Xt0),as.double(Xt1),as.double(Xt2),
-     XtildePerm=as.double(XtildePerm), as.integer(B),as.integer(N),as.integer(L),
+  Xtildeperm <- rep(0,N)
+  t.min=.C("fastperm3",as.double(Xt0),as.double(Xt1),as.double(Xt2),
+     as.double(Xtildeperm), as.integer(B),as.integer(N),as.integer(L),
      maxvec=as.double(t.max),minvec=as.double(t.min),PACKAGE="flash")$minvec
-  return(list(MinVec,MaxVec,Xt0,Xt1,Xt2,Xtildeperm))
+  t.max=.C("fastperm3",as.double(Xt0),as.double(Xt1),as.double(Xt2),
+     as.double(Xtildeperm), as.integer(B),as.integer(N),as.integer(L),
+     maxvec=as.double(t.max),minvec=as.double(t.min),PACKAGE="flash")$maxvec
+  return(list(t.min,t.max))
 }
--- src/fastperm.c
+++ src/fastperm.c
@@ -5,24 +5,24 @@
 
 void fastperm3(double *X0,double *X1,double *X2,double *XtildePerm,int *B,int *N,int *L,
                double *maxvec, double *minvec){
-  int i=0,j=0,m1=0,m2=0,BB=*B,NN=*N,ind=0,LL=*L;
-  double tmax=0,tmin=0;
+  int i=0,j=0,m1=0,m2=0,BB=*B,NN=*N,ind,LL=*L;
+  double tmax=-100,tmin=100,Xperm;
   for (m1=0;m1<BB;m1++){
     for (i=0;i<NN;i++) {
-      ind=(i+LL*m1) % NN;
-      ind=((ind == 0) ? NN:ind);
+      ind=((i+LL*m1) % NN);
       XtildePerm[i]=X0[i]+X1[ind];
     }
     for (m2=0;m2<BB;m2++){
       for (j=0;j<NN;j++){
-        ind=(j+LL*m2) % NN;
-        ind=((ind == 0) ? NN:ind);
-        XtildePerm[j]+=X2[ind];
-        tmin=((XtildePerm[j] < tmin) ? XtildePerm[j]:tmin);
-        tmax=((XtildePerm[j] > tmax) ? XtildePerm[j]:tmax);
+        ind=((j+LL*m2) % NN);
+        Xperm =XtildePerm[j]+X2[ind];   
+        tmin=((Xperm < tmin) ? Xperm:tmin);
+        tmax=((Xperm > tmax) ? Xperm:tmax);
       }
       maxvec[m1*BB+m2]=tmax;
       minvec[m1*BB+m2]=tmin;
+      tmax=-100;  /*wait to be edited,need be cleaned*/
+      tmin=100;   /*wait to be edited,need be cleaned*/
     }
   }
 }
