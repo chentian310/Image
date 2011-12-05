4bbea6dc7ea0bae2cbde5c32c4be22d051cf8f55
Tian Chen <tian_chen@urmc.rochester.edu>
2011-12-05 14:54:45 -0500

    changes made: (1)fixed fastperm.C (2)add TempSmooth(..) to auxiliary.R, need future improvement (3)add GaussFilterMat(..) and shift(..) to auxiliary.R (4)Changed NAMESPACE, adding GaussFilterMat, shift in export(...)

--- R/auxiliary.R
+++ R/auxiliary.R
@@ -1,13 +1,14 @@
 ## fastperm3.R version
-fastperm3.R <- function(X,t.min,t.max){
+fastperm3.R <- function(X){
   N<-length(X);B<-N/3;ind<-seq(0,N-1,3)
   XX <- matrix(X,ncol=3,byrow=T)
   Xtilde <- matrix(0,nrow=N,ncol=3)
   for (l in 1:3){
     Xtilde[,l] <- as.vector(kermat[,ind1+l]%*%XX[,l])
   }
-  Xt0 <- Xtilde[,1];Xt1 <- Xtilde[,2]; Xt2 <- Xtilde[,3]
+  Xt0 <- Xtilde[,1];Xt1 <- Xtilde[,2]; Xt2 <- Xtilde[,3] # should be changed if L is not 3
   ## R version
+  t.min <- rep(0,B^2);t.max <- rep(0,B^2)
   for (m1 in 1:B){
     Xt1 <- shift(Xt1,3)
     sums01 <- Xt0 + Xt1
@@ -24,16 +25,17 @@ fastperm3.R <- function(X,t.min,t.max){
 
 
 ## fastperm3.C version
-fastperm3.C <- function(X,t.mim,t.max,L=3){
+fastperm3.C <- function(X,L=3){ # t.min, t.max seems not necessary here
   N<-length(X);B<-N/L;ind<-seq(0,N-1,3)
   XX <- matrix(X,ncol=3,byrow=T)
   Xtilde <- matrix(0,nrow=N,ncol=3)
   for (l in 1:3){
-    Xtilde[,l] <- as.vector(kermat[,ind1+l]%*%XX[,l])
+    Xtilde[,l] <- as.vector(kermat[,ind1+l]%*%XX[,l])     # uses "kermat",better as a input
   }
-  Xt0 <- Xtilde[,1];Xt1 <- Xtilde[,2]; Xt2 <- Xtilde[,3]
+  Xt0 <- Xtilde[,1];Xt1 <- Xtilde[,2]; Xt2 <- Xtilde[,3]  # need to be more flexible
   ## implemented in C
   Xtildeperm <- rep(0,N)
+  t.min <- rep(0,B^2);t.max <- rep(0,B^2)
   t.min=.C("fastperm3",as.double(Xt0),as.double(Xt1),as.double(Xt2),
      as.double(Xtildeperm), as.integer(B),as.integer(N),as.integer(L),
      maxvec=as.double(t.max),minvec=as.double(t.min),PACKAGE="flash")$minvec
@@ -42,3 +44,24 @@ fastperm3.C <- function(X,t.mim,t.max,L=3){
      maxvec=as.double(t.max),minvec=as.double(t.min),PACKAGE="flash")$maxvec
   return(list(t.min,t.max))
 }
+
+
+## The Gauss Filter Matrix
+GaussFilterMat <- function(N,sigma=5,cut=4){
+   sigma <- 5.0; cutoff <- min(cut*sigma,N-1);tt<-0:(N-1)
+   kern1 <- rep(0,N)
+   kern1[1:(cutoff+1)] <- dnorm(0:cutoff,sd=sigma)
+   kern1[(N-cutoff+1):N] <- kern1[(N-cutoff+1):N]+dnorm(-cutoff:-1,sd=sigma)
+   kermat <- sapply(tt,shift,x=kern1)
+   return(kermat)
+ }
+
+
+## A shift function for permutation
+shift <-  function(x,i=1){
+      n<-length(x);i<-i%%n
+    if (i==0){
+         return(x)
+       }
+    return(x[c((n-i+1):n,1:(n-i))])
+  }
--- src/fastperm.c
+++ src/fastperm.c
@@ -6,13 +6,16 @@
 void fastperm3(double *X0,double *X1,double *X2,double *XtildePerm,int *B,int *N,int *L,
                double *maxvec, double *minvec){
   int i=0,j=0,m1=0,m2=0,BB=*B,NN=*N,ind,LL=*L;
-  double tmax=-100,tmin=100,Xperm;
+  double tmax,tmin,Xperm;
   for (m1=0;m1<BB;m1++){
     for (i=0;i<NN;i++) {
       ind=((i+LL*m1) % NN);
       XtildePerm[i]=X0[i]+X1[ind];
     }
     for (m2=0;m2<BB;m2++){
+      tmin=XtildePerm[0]+X2[LL*m2];
+      tmax=XtildePerm[0]+X2[LL*m2];
+
       for (j=0;j<NN;j++){
         ind=((j+LL*m2) % NN);
         Xperm =XtildePerm[j]+X2[ind];   
@@ -21,8 +24,6 @@ void fastperm3(double *X0,double *X1,double *X2,double *XtildePerm,int *B,int *N
       }
       maxvec[m1*BB+m2]=tmax;
       minvec[m1*BB+m2]=tmin;
-      tmax=-100;  /*wait to be edited,need be cleaned*/
-      tmin=100;   /*wait to be edited,need be cleaned*/
     }
   }
 }
